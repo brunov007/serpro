8718e00e7dd21652b4c42351ab77e1dfa370ddf4
System Administrator <brunomarcio2006@gmail.com>
2023-04-28 17:15:23 -0300

    header da pagina main

---
+++ serpro-front/src/components/HeaderMain.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import avatarWoman from '../assets/avatar-woman.svg'
+
+export function HeaderMain() {
+  
+  return (
+    <header className="container flex items-center justify-between mx-auto bg-[#00000041] p-3 px-12 rounded-3xl blur-image">
+      <Link to="/"><img src="https://www.serpro.gov.br/++resource++serpro.portalserprotema/img/logo-serpro.png" alt="" /></Link>
+
+      <div>
+        <div className="md:flex gap-5">
+        <div className="md:flex mt-2 text-white">
+          <span>User n11223j3k2n1</span>
+        </div>
+          <img
+            className="h-8 w-8  md:w-12 md:h-12 cursor-pointer"
+            src={avatarWoman}
+            alt='foto do usuario'
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
