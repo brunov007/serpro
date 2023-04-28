2967fbae2203c1de7115c6f2a6a809d76d5059c9
System Administrator <brunomarcio2006@gmail.com>
2023-04-28 17:16:46 -0300

    pagina com main (html e algumas navegacoes)

--- serpro-front/src/pages/Main/index.js
+++ serpro-front/src/pages/Main/index.js
@@ -1,12 +1,39 @@
 //import { useEffect, useState } from "react";
-
-//import api from "../../services/api";
+import MaterialIcon from 'material-icons-react';
+import { HeaderMain } from "../../components/HeaderMain";
 
 export function Main() {
 
   return (
     <>
-      main
+      <div className="body-main h-screen">
+        <div className="p-12">
+          <HeaderMain />
+          <div class="flex flex-row gap-6 mt-10">
+            <div class="flex flex-col h-fit w-20 p-20 gap-10 items-center bg-white rounded-lg">
+              
+              <div className="flex flex-col items-center rounded-lg bg-blue-50">
+               <MaterialIcon icon="dashboard" size={50}/>
+               <h4>Funcionalidade 1</h4>
+              </div>
+
+              <div className="flex flex-col items-center rounded-lg bg-blue-50">
+               <MaterialIcon icon="dataset" size={50}/>
+               <h4>Funcionalidade 2</h4>
+              </div>
+
+              <div className="flex flex-col items-center rounded-lg bg-blue-50">
+               <MaterialIcon icon="view_kanban" size={50}/>
+               <h4>Funcionalidade 3</h4>
+              </div>
+            </div>
+            
+            <div class="flex-auto w-80 bg-white rounded-lg h-screen">
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
