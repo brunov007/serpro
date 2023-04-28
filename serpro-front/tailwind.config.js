a6c8cbcd95ffb8749fa37cfb8b8656fda34b1c9c
System Administrator <brunomarcio2006@gmail.com>
2023-04-28 01:11:52 -0300

    tailind css assets

---
+++ serpro-front/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.js"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/src/assets/bg.png')",
+      },
+      fontFamily: {
+        'gilroy-medium': ['Gilroy-Medium', 'sans-serif'],
+        'gilroy-bold': ['Gilroy-Bold', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+  ],
+}
