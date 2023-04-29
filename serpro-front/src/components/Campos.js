f081da763e8d7a33fed3dadf63c3b64e99ad3005
System Administrator <brunomarcio2006@gmail.com>
2023-04-29 00:26:59 -0300

    botoes e script addEmail

--- serpro-front/src/components/Campos.js
+++ serpro-front/src/components/Campos.js
@@ -1,7 +1,34 @@
+import MaterialIcon from 'material-icons-react';
+
 export function Campos(){
+
+    const addEmailField = () => {
+        const myForm = document.getElementById("email-list");
+        
+        const newEmailField = document.createElement("input");
+        //newEmailField.type = "email";
+        newEmailField.name = "email_field";
+        newEmailField.setAttribute("required", "");
+        newEmailField.classList.add("input-field");
+        newEmailField.classList.add("mt-4");
+
+        // insert element
+        myForm.appendChild(newEmailField);
+    }
+
     return (
-        <div>
-            a
+        <div className="">
+            <form id="form" action="" method="POST" className="">
+                <fieldset class="inputs-set" id="email-list">
+                    <input class="input-field" name="email_field[]" required placeholder="dado"/>
+                </fieldset>
+                <div className="flex flex-col gap-4 items-center">
+                    <button onClick={addEmailField} className="bg-white rounded-full w-fit h-fit">
+                        <MaterialIcon icon="add" size={40}/>
+                    </button>
+                    <button class="btn-submit w-80" type="submit">SUBMIT</button>
+                </div>
+            </form>
         </div>
     );
 }
