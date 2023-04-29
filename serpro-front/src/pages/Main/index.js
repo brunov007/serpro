dfc2f3de9404b4136bf0444203f525d63001d5e0
System Administrator <brunomarcio2006@gmail.com>
2023-04-28 22:30:00 -0300

    trocar cor de backgorund

--- serpro-front/src/pages/Main/index.js
+++ serpro-front/src/pages/Main/index.js
@@ -11,25 +11,25 @@ export function Main() {
         <div className="p-12">
           <HeaderMain />
           <div class="flex flex-row gap-4 mt-10">
-            <div class="flex flex-col h-fit w-auto p-10 gap-8 items-center bg-white rounded-lg">
+            <div class="flex flex-col h-fit w-auto p-10 gap-8 items-center bg-[#00000041] rounded-lg">
               
-              <div className="flex flex-col p-3 items-center rounded-lg bg-blue-50">
+              <div className="flex flex-col p-3 items-center rounded-lg bg-indigo-100">
                <MaterialIcon icon="dashboard" size={50}/>
                <h4>Campos</h4>
               </div>
 
-              <div className="flex flex-col p-3 items-center rounded-lg bg-blue-50">
+              <div className="flex flex-col p-3 items-center rounded-lg bg-indigo-100">
                <MaterialIcon icon="dataset" size={50}/>
                <h4>Excel</h4>
               </div>
 
-              <div className="flex flex-col p-3 items-center rounded-lg bg-blue-50">
+              <div className="flex flex-col p-3 items-center rounded-lg bg-indigo-100">
                <MaterialIcon icon="view_kanban" size={50}/>
                <h4>Tabela</h4>
               </div>
             </div>
             
-            <div class="flex-auto w-80 bg-white rounded-lg h-screen">
+            <div class="flex-auto w-80 bg-[#00000041] rounded-lg h-screen">
               {
                 // Tela pra cada funcionalidade selecionada
               }
