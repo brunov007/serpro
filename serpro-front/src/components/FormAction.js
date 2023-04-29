99708ab9d152808b0a4db7495b08946b1ce586c6
System Administrator <brunomarcio2006@gmail.com>
2023-04-29 04:33:03 -0300

    classname sendo passado dinamicamente

--- serpro-front/src/components/FormAction.js
+++ serpro-front/src/components/FormAction.js
@@ -2,7 +2,8 @@ export function FormAction({
     handleSubmit,
     type='Button',
     action='submit',
-    text
+    text,
+    className
 }){
     return(
         <>
@@ -10,7 +11,7 @@ export function FormAction({
             type==='Button' ?
             <button
                 type={action}
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 mt-10"
+                className={className}
                 onSubmit={handleSubmit}
             >
 
