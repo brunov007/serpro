cf89cde7b81ded5a98244df3800614d74fc3f043
System Administrator <brunomarcio2006@gmail.com>
2023-04-30 01:32:50 -0300

    comeco csv

---
+++ serpro-front/src/components/Csv.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export function Csv(){
+    const [isLoading, setIsLoading] = useState(false)
+
+    return (
+        <>
+            <h1>a</h1>
+        </>
+    );
+}
