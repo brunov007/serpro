0b790bedc25e4e5279be0496af3dc3f48dc49097
System Administrator <brunomarcio2006@gmail.com>
2023-04-30 00:22:33 -0300

    configuracao cors

--- serpro-back/src/main.ts
+++ serpro-back/src/main.ts
@@ -1,9 +1,20 @@
+import { NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const appOptions: NestApplicationOptions = {
+    cors: { 
+      origin: "*", 
+      methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
+      allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", 
+      "Host", "User-Agent"],
+      preflightContinue: false
+
+    }
+  }
+  const app = await NestFactory.create(AppModule, appOptions);
   app.setGlobalPrefix('api') 
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();
