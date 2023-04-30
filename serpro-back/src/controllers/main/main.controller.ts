8cee70c234970caeb98c57b9396604ba0e6e3bfe
System Administrator <brunomarcio2006@gmail.com>
2023-04-30 02:49:58 -0300

    chatresponse

--- serpro-back/src/controllers/main/main.controller.ts
+++ serpro-back/src/controllers/main/main.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, HttpCode, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { OK } from 'http-status';
-import { BaseResponse } from 'src/models/reponse/base-response';
+import { ChatResponse } from 'src/models/reponse/chat-response.interface';
 import { MainRequest } from 'src/models/request/main-request';
 
 @Controller('main')
@@ -9,19 +9,19 @@ export class MainController {
 
     @Post('data')
     @HttpCode(OK)
-    async data(@Body() body: MainRequest): Promise<BaseResponse> {
+    async data(@Body() body: MainRequest): Promise<ChatResponse> {
         console.log(body)
-        return {status: true}
+        return {text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", status: true}
     }
 
     @Post('upload/csv')
     @HttpCode(OK)
     @UseInterceptors(FileInterceptor('file'))
-    async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<BaseResponse> {
+    async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<ChatResponse> {
+        console.log(file)
         if(!file) return {status: false}
         if(file.mimetype !== 'text/csv') return {status: false}
-        console.log(file)
         //file.buffer
-        return {status: true}
+        return {text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", status: true}
     }
 }
