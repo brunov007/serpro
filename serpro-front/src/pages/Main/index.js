30346451f7a913c20a8e3bf304540b95a17efec0
System Administrator <brunomarcio2006@gmail.com>
2023-04-30 01:32:31 -0300

    controle de tela ao clicar no icone

--- serpro-front/src/pages/Main/index.js
+++ serpro-front/src/pages/Main/index.js
@@ -1,9 +1,29 @@
 //import { useEffect, useState } from "react";
 import MaterialIcon from 'material-icons-react';
+import { useState } from 'react';
 import { Campos } from '../../components/Campos';
+import { Csv } from '../../components/Csv';
 import { HeaderMain } from "../../components/HeaderMain";
 
 export function Main() {
+
+  const [current, setCurrent] = useState(0)
+
+  const set = event => {
+    switch(event.target.id){
+      case "dashboard":
+        setCurrent(0)
+        break
+      case "dataset":
+        setCurrent(1)
+        break
+      case "view_kanban":
+        setCurrent(2)
+        break
+      default:
+        setCurrent(0)
+    }
+  }
 
   return (
     <>
@@ -13,27 +33,24 @@ export function Main() {
           <div className="flex flex-row gap-4 mt-10 blur-image-main">
             <div className="flex flex-col h-100 w-auto p-10 gap-8 items-center bg-[#00000041] rounded-lg">
               
-              <button className="custom-button-main flex flex-col p-2 items-center">
-               <MaterialIcon icon="dashboard" size={50} color="#000010"/>
-               <h4>Campos</h4>
+              <button id= "dashboard" className="custom-button-main flex flex-col p-2 items-center" onClick={set}>
+               <MaterialIcon id= "dashboard" icon="dashboard" size={50} color="#000010"/>
+               Campos
               </button>
 
-              <button className="custom-button-main flex flex-col p-2 items-center">
-               <MaterialIcon icon="dataset" size={50} color="#000010"/>
-               <h4>Excel</h4>
+              <button id= "dataset" className="custom-button-main flex flex-col p-2 items-center" onClick={set}>
+               <MaterialIcon id= "dataset"  icon="dataset" size={50} color="#000010"/>
+               Excel
               </button>
 
-              <button className="custom-button-main flex flex-col p-2 items-center">
-               <MaterialIcon icon="view_kanban" size={50} color="#000010"/>
-               <h4>Tabela</h4>
+              <button id= "view_kanban" className="custom-button-main flex flex-col p-2 items-center" onClick={set}>
+               <MaterialIcon id= "view_kanban" icon="view_kanban" size={50} color="#000010"/>
+               Tabela
               </button>
             </div>
             
             <div className="flex-auto bg-[#00000041] rounded-lg h-100 p-10">
-              {
-                // Tela pra cada funcionalidade selecionada
-              }
-              <Campos/>
+              {current === 0 ? <Campos/> : current === 1 ? <Csv/> : null }
             </div>
             <div className="flex-auto bg-[#00000049] rounded-lg h-100 p-10">
               
